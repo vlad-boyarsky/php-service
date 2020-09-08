b066f84046453b8ca0075207a40b432bc9b27404
hyper-vlad <hypervlados@gmail.com>
2020-09-08 16:43:12 +0300

    Service rewrite pcntl

--- index.php
+++ index.php
@@ -1,6 +1,32 @@
 <?php
 
-    if($_SESSION['provide_entry_user']){
-        unset($_SESSION['provide_entry_user']);
+$isEnable = true;
+
+$numbersData;
+
+$pid = pcntl_fork();
+
+if ($pid == -1) {
+
+    die('ERROR to make fork process' . PHP_EOL);
+
+} elseif ($pid) {
+
+    die('OK Destroy parent process' . PHP_EOL);
+
+} else {
+
+    while($isEnable) {
+        
+        for ($i = 0; $i < 10; $i++) {
+            
+            file_put_contents('/var/www/' . current_date . '.txt', 'Time: '. date('Y-m-s') .' updated by service' );
+            sleep(2);
+        }
+        
+        $isEnable = false;
     }
+}
+
+posix_setsid();
     
